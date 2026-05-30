f4551df2d0dc89d46a441fcb54e2a793b406e59f
NacisCric-2908 <juandiegoc2908@gmail.com>
2026-05-29 21:24:48 -0500

    solucion conflictos

--- airflow/dags/silver_processing_dag.py
+++ airflow/dags/silver_processing_dag.py
@@ -169,7 +169,7 @@ with DAG(
     
     trigger_gold = TriggerDagRunOperator(
         task_id="trigger_gold_webscraping",
-        trigger_dag_id="gold_processing_dag",
+        trigger_dag_id="gold_webscraping",
         wait_for_completion=False,
     )
 
@@ -195,7 +195,7 @@ with DAG(
     
     trigger_gold = TriggerDagRunOperator(
         task_id="trigger_gold_twitter",
-        trigger_dag_id="gold_processing_dag",
+        trigger_dag_id="gold_twitter",
         wait_for_completion=False,
     )
 
